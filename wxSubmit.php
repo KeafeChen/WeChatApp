93c8b6a95b152c394106d330c635fc321de1f26b
KeafeChen <31990264+KeafeChen@users.noreply.github.com>
2017-09-26 22:17:12 -0400

    wxSubmit New

--- wxSubmit.php
+++ wxSubmit.php
@@ -17,11 +17,34 @@
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		$num = $_POST["id"];
 	}
-	echo $num;
+	//echo $num;
+	$id=$_POST['id'];
+	$group1_male=$_POST['group1_male'];
+	$group2_male=$_POST['group2_male'];
+	$group3_male=$_POST['group3_male'];
+	$group1_female=$_POST['group1_female'];
+	$group2_female=$_POST['group2_female'];
+	$group3_female=$_POST['group3_female'];
 
-	$smt = $pdo -> prepare("SELECT id FROM vote_round1 where id=:ID");
+	$sql = "INSERT INTO vote_round1((id, group1_male, group2_male, group3_male, group1_female, group2_female, group3_female) VALUES ('{$id}','{$group1_male}','{$group2_male}','{$groupe_male}','{$group1_female}','{$group2_female}','{$group3_female}')";
+	if($pdo->query($sql)){
+		echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";
+	}
+	else{
+		echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
+	}
+
+
+	$smt = $pdo -> prepare("SELECT id, group1_male, group2_male, group3_male, group1_female, group2_female, group3_female FROM vote_round1 where id=:ID, group1_male=:male1, group2_male=:male2, group3_male=male3, group1_female=:female1, group2_female=:female2, group3_female=:female3");
 	$smt -> bindValue(":ID", $id);
+	$smt -> bindValue(":male1", $group1_male);
+	$smt -> bindValue(":male2", $group2_male);
+	$smt -> bindValue(":male3", $group3_male);
+	$smt -> bindValue(":female1", $group1_female);
+	$smt -> bindValue(":female2", $group2_female);
+	$smt -> bindValue(":female3", $group3_female);
 	$smt -> execute();
+
 
 	$result = $smt -> fetch();
 	$id = $result["id"];
